fc66b243147c16fb785907b9f75c158504c41c71
Li-fan-se_Bohdan <lifanseb@gmail.com>
2025-12-13 21:55:28 +0200

    розробили реалізацію першого тесту

---
+++ SmartFridge/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridge
+{
+    public class Product
+    {
+        public string Name { get; }
+        public DateTime ExpirationDate { get; }
+
+        public Product(string name, DateTime expirationDate)
+        {
+            Name = name;
+            ExpirationDate = expirationDate;
+        }
+    }
+}
